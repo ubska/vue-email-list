751c2aa958b74e302c97c2ffc56da7d6310b3be8
Oleksandra Ubska <alessandraubska@gmail.com>
2024-06-27 18:15:52 +0200

    add css style

--- js/main.js
+++ js/main.js
@@ -14,14 +14,21 @@ createApp({
     methods: {
 
     },
+
+     // Metodo mounted viene eseguito una volta che l'app è stata montata
     mounted() {
+        // richiesta all'API per 10 volte
         for (let i = 0; i < 10; i++){
-           axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then (risposta => {
-            const result = risposta.data.response;
-            console.log(result);
-            this.emails.push(result);
-        }); 
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                
+                 // Estrarre l'email dalla risposta dell'API
+                .then(risposta => {
+                const result = risposta.data.response;
+                console.log(result);
+                    
+                // Aggiungere l'email all'array emails
+                this.emails.push(result);
+            }); 
         }
         
     }
